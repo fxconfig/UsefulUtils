c8e6034c29d4e49b14429e485d78cbe8c5bbaf7e
fxconfig <peter1946073945@gmail.com>
2023-11-14 21:43:06 +0800

    Update CharCodingConversion.h

--- CharCodingConversion.h
+++ CharCodingConversion.h
@@ -1,113 +1,178 @@
-#pragma once
-#include <Windows.h>
 #include <string>
-using namespace std;
+#include <locale>
+#include <codecvt>
+#include <iomanip>
 
-static wstring ANSIToUnicode(const string& str)
+
+template<class Facet>
+struct deletable_facet : Facet
 {
-	int len = 0;
-	len = str.length();
-	int unicodeLen = ::MultiByteToWideChar(CP_ACP,
-		0,
-		str.c_str(),
-		-1,
-		NULL,
-		0);
-	wchar_t * pUnicode;
-	pUnicode = new wchar_t[unicodeLen + 1];
-	memset(pUnicode, 0, (unicodeLen + 1) * sizeof(wchar_t));
-	::MultiByteToWideChar(CP_ACP,
-		0,
-		str.c_str(),
-		-1,
-		(LPWSTR)pUnicode,
-		unicodeLen);
-	wstring rt;
-	rt = (wchar_t*)pUnicode;
-	delete pUnicode;
-	return rt;
+	template<class ...Args>
+	deletable_facet(Args&& ...args)
+		: Facet(std::forward<Args>(args)...) {}
+	~deletable_facet() {}
+};
+using mbs_facet_t = deletable_facet<std::codecvt_byname<wchar_t, char, std::mbstate_t>>;
+
+#if defined(_WIN32) || defined(_WIN64)
+#define GBK_LOCALE_NAME  ".936"
+#else
+#define GBK_LOCALE_NAME  "zh_CN.GBK"
+#endif
+
+static std::string ToString(const std::wstring& wstr)
+{
+	//std::locale::global(std::locale(""));
+	const mbs_facet_t& cvt = std::use_facet<mbs_facet_t>(std::locale());
+	std::wstring_convert<mbs_facet_t> converter(&cvt);
+	std::string str = converter.to_bytes(wstr);
+	return str;
 }
 
-static string UnicodeToANSI(const wstring& str)
+static std::wstring ToWString(const std::string& str)
 {
-	char*     pElementText;
-	int    iTextLen;
-	// wide char to multi char
-	iTextLen = WideCharToMultiByte(CP_ACP,
-		0,
-		str.c_str(),
-		-1,
-		NULL,
-		0,
-		NULL,
-		NULL);
-	pElementText = new char[iTextLen + 1];
-	memset((void*)pElementText, 0, sizeof(char) * (iTextLen + 1));
-	::WideCharToMultiByte(CP_ACP,
-		0,
-		str.c_str(),
-		-1,
-		pElementText,
-		iTextLen,
-		NULL,
-		NULL);
-	string strText;
-	strText = pElementText;
-	delete[] pElementText;
-	return strText;
+	//std::locale::global(std::locale(""));
+	const mbs_facet_t& cvt = std::use_facet<mbs_facet_t>(std::locale());
+	std::wstring_convert<mbs_facet_t> converter(&cvt);
+	std::wstring wstr = converter.from_bytes(str);
+	return wstr;
 }
 
-static wstring UTF8ToUnicode(const string& str)
+static std::string ToGBK(const std::wstring& wstr)
 {
-	int  len = 0;
-	len = str.length();
-	int  unicodeLen = ::MultiByteToWideChar(CP_UTF8,
-		0,
-		str.c_str(),
-		-1,
-		NULL,
-		0);
-	wchar_t *  pUnicode;
-	pUnicode = new  wchar_t[unicodeLen + 1];
-	memset(pUnicode, 0, (unicodeLen + 1) * sizeof(wchar_t));
-	::MultiByteToWideChar(CP_UTF8,
-		0,
-		str.c_str(),
-		-1,
-		(LPWSTR)pUnicode,
-		unicodeLen);
-	wstring  rt;
-	rt = (wchar_t*)pUnicode;
-	delete  pUnicode;
-
-	return  rt;
+	std::wstring_convert<mbs_facet_t> conv(new mbs_facet_t(GBK_LOCALE_NAME));
+	std::string  str = conv.to_bytes(wstr);
+	return str;
 }
 
-static string UnicodeToUTF8(const wstring& str)
+static std::wstring FromGBK(const std::string& str)
 {
-	char*     pElementText;
-	int    iTextLen;
-	// wide char to multi char
-	iTextLen = WideCharToMultiByte(CP_UTF8,
-		0,
-		str.c_str(),
-		-1,
-		NULL,
-		0,
-		NULL,
-		NULL);
-	pElementText = new char[iTextLen + 1];
-	memset((void*)pElementText, 0, sizeof(char) * (iTextLen + 1));
-	::WideCharToMultiByte(CP_UTF8,
-		0,
-		str.c_str(),
-		-1,
-		pElementText,
-		iTextLen,
-		NULL,
-		NULL);
-	string strText;
-	strText = pElementText;
-	delete[] pElementText;
-	return strText;
+	std::wstring_convert<mbs_facet_t> conv(new mbs_facet_t(GBK_LOCALE_NAME));
+	std::wstring wstr = conv.from_bytes(str);
+	return wstr;
+}
+
+static std::string ToUTF8(const std::wstring& wstr)
+{
+#if defined(__CYGWIN__) || defined(__MINGW32__) || defined(__MINGW64__)
+	std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
+	return convert.to_bytes((char16_t*)wstr.data(), (char16_t*)wstr.data() + wstr.size());
+#elif defined(unix) || defined(__unix) || defined(__unix__) || defined(__APPLE__)
+	std::wstring_convert<std::codecvt_utf8<wchar_t>> convert;
+	return convert.to_bytes(wstr.data(), wstr.data() + wstr.size());
+#elif defined(_WIN32) || defined(_WIN64)
+	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> convert;
+	return convert.to_bytes(wstr.data(), wstr.data() + wstr.size());
+#endif
+}
+
+static std::wstring FromUTF8(const std::string& str)
+{
+#if defined(__CYGWIN__) || defined(__MINGW32__) || defined(__MINGW64__)
+	std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
+	auto tmp = convert.from_bytes(str.data(), str.data() + str.size());
+	return std::wstring(tmp.data(), tmp.data() + tmp.size());
+#elif defined(unix) || defined(__unix) || defined(__unix__) || defined(__APPLE__)
+	std::wstring_convert<std::codecvt_utf8<wchar_t>> convert;
+	return convert.from_bytes(str.data(), str.data() + str.size());
+#elif defined(_WIN32) || defined(_WIN64)
+	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> convert;
+	return convert.from_bytes(str.data(), str.data() + str.size());
+#endif
+}
+
+static std::string GBKToUTF8(const std::string& str)
+{
+	return ToUTF8(FromGBK(str));
+}
+
+static std::string UTF8ToGBK(const std::string& str)
+{
+	return ToGBK(FromUTF8(str));
+}
+
+static std::u16string UTF8toUTF16(const std::string& str)
+{
+#if defined(_MSC_VER)
+	std::wstring_convert<std::codecvt_utf8_utf16<uint16_t>, uint16_t> convert;
+	auto tmp = convert.from_bytes(str.data(), str.data() + str.size());
+	return std::u16string(tmp.data(), tmp.data() + tmp.size());
+#else
+	std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
+	return convert.from_bytes(str.data(), str.data() + str.size());
+#endif
+}
+
+static std::u32string UTF8toUTF32(const std::string& str)
+{
+#if defined(_MSC_VER)
+	std::wstring_convert<std::codecvt_utf8<uint32_t>, uint32_t> convert;
+	auto tmp = convert.from_bytes(str.data(), str.data() + str.size());
+	return std::u32string(tmp.data(), tmp.data() + tmp.size());
+#else
+	std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> convert;
+	return convert.from_bytes(str.data(), str.data() + str.size());
+#endif
+}
+
+static std::string UTF16toUTF8(const std::u16string& str)
+{
+#if defined(_MSC_VER)
+	std::wstring_convert<std::codecvt_utf8_utf16<uint16_t>, uint16_t> convert;
+	return convert.to_bytes((uint16_t*)str.data(), (uint16_t*)str.data() + str.size());
+#else
+	std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
+	return convert.to_bytes(str.data(), str.data() + str.size());
+#endif
+}
+
+static std::u32string UTF16toUTF32(const std::u16string& str)
+{
+	std::string bytes;
+	bytes.reserve(str.size() * 2);
+
+	for (const char16_t ch : str)
+	{
+		bytes.push_back((uint8_t)(ch / 256));
+		bytes.push_back((uint8_t)(ch % 256));
+	}
+
+#if defined(_MSC_VER)
+	std::wstring_convert<std::codecvt_utf16<uint32_t>, uint32_t> convert;
+	auto tmp = convert.from_bytes(bytes);
+	return std::u32string(tmp.data(), tmp.data() + tmp.size());
+#else
+	std::wstring_convert<std::codecvt_utf16<char32_t>, char32_t> convert;
+	return convert.from_bytes(bytes);
+#endif
+}
+
+static std::string UTF32toUTF8(const std::u32string& str)
+{
+#if defined(_MSC_VER)
+	std::wstring_convert<std::codecvt_utf8<uint32_t>, uint32_t> convert;
+	return convert.to_bytes((uint32_t*)str.data(), (uint32_t*)str.data() + str.size());
+#else
+	std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> convert;
+	return convert.to_bytes(str.data(), str.data() + str.size());
+#endif
+}
+
+static std::u16string UTF32toUTF16(const std::u32string& str)
+{
+#if defined(_MSC_VER)
+	std::wstring_convert<std::codecvt_utf16<uint32_t>, uint32_t> convert;
+	std::string bytes = convert.to_bytes((uint32_t*)str.data(), (uint32_t*)str.data() + str.size());
+#else
+	std::wstring_convert<std::codecvt_utf16<char32_t>, char32_t> convert;
+	std::string bytes = convert.to_bytes(str.data(), str.data() + str.size());
+#endif
+
+	std::u16string result;
+	result.reserve(bytes.size() / 2);
+
+	for (size_t i = 0; i < bytes.size(); i += 2)
+		result.push_back((char16_t)((uint8_t)(bytes[i]) * 256 + (uint8_t)(bytes[i + 1])));
+
+	return result;
 }
